0971e6a6104d068e2db987d3c12f66cb593359cc
drfreder <m.frederickson@utoronto.ca>
2026-04-28 21:16:29 -0700

    Fix colors in phylogeny heatmap

--- code/Figure_S1.R
+++ code/Figure_S1.R
@@ -21,31 +21,42 @@ mytree <- read.tree(here("phylogeny/phylogeny_polytomy_removed.tre"))
 #Drop tree tips not in dataset
 tree_pruned <- drop.tip(mytree, setdiff(mytree$tip.label, data$species))
 
-# Make tree figure
-p <- ggtree(tree_pruned, linewidth=0.1, layout="circ")
-
-#Discrete traits
-tree_data_1 <- as.data.frame(data[,c("species", "EFN", "fixer")])
-rownames(tree_data_1) <- tree_data_1[, c("species")]
-tree_data_1 <- tree_data_1[,-1]
+#EFN trait
+tree_data_1 <- as.data.frame(data[,c("species", "EFN")])
 tree_data_1$EFN <- as.factor(tree_data_1$EFN)
-tree_data_1$fixer <- as.factor(tree_data_1$fixer)
-colnames(tree_data_1) <- c("EFN", "Rhizobia")
+rownames(tree_data_1) <- tree_data_1[, c("species")]
+tree_data_1 <- tree_data_1[,-1, drop = FALSE]
+colnames(tree_data_1) <- c("EFN")
+
+#Rhizobia trait
+tree_data_2 <- as.data.frame(data[,c("species", "fixer")])
+tree_data_2$fixer <- as.factor(tree_data_2$fixer)
+rownames(tree_data_2) <- tree_data_2[, c("species")]
+tree_data_2 <- tree_data_2[,-1, drop = FALSE]
+colnames(tree_data_2) <- c("Rhizobia")
 
 #Continuous traits
-tree_data_2 <- as.data.frame(data[,c("species", "precip_range", "temp_range", "nitro_range")])
-rownames(tree_data_2) <- tree_data_2[, c("species")]
-tree_data_2 <- tree_data_2[,-1]
-tree_data_2$precip_range <- scale(log(tree_data_2$precip_range), center=T, scale=T)
-tree_data_2$temp_range <- scale(log(tree_data_2$temp_range), center=T, scale=T)
-tree_data_2$nitro_range <- scale(log(tree_data_2$nitro_range), center=T, scale=T)
-colnames(tree_data_2) <- c("Precip", "Temp", "N")
+tree_data_3 <- as.data.frame(data[,c("species", "precip_range", "temp_range", "nitro_range")])
+rownames(tree_data_3) <- tree_data_3[, c("species")]
+tree_data_3 <- tree_data_3[,-1]
+tree_data_3$precip_range <- scale(log(tree_data_3$precip_range), center=T, scale=T)
+tree_data_3$temp_range <- scale(log(tree_data_3$temp_range), center=T, scale=T)
+tree_data_3$nitro_range <- scale(log(tree_data_3$nitro_range), center=T, scale=T)
+colnames(tree_data_3) <- c("Precip", "Temp", "N")
 
-#Make figure
-p1<- gheatmap(p, tree_data_2, width=.5, offset=28, color=NA, colnames_angle=45, font.size = 2)+
-  scale_fill_continuous(type="viridis", name="Niche breadth")
-p2 <- p1 + new_scale_fill()
-p3 <- gheatmap(p2, data = tree_data_1, width=0.3, offset=0.02, color=NA, colnames_angle = 45, font.size=2)+
-  scale_fill_manual(values = c("#0E84B4FF", "#B50A2AFF"), name="Trait")
-p3
-ggsave("phylogeny/tree_heatmap.pdf", p3)
+# Base tree
+p <- ggtree(tree_pruned, linewidth=0.1, layout="circ")
+
+# First heatmap
+p2 <- gheatmap(p, tree_data_3, width=.5, offset=28, color=NA, colnames_angle=45, font.size=2) + scale_fill_viridis_c(name="Niche breadth")
+
+# Second heatmap (new scale)
+p3 <- gheatmap(p2 + new_scale_fill(), tree_data_1, width=0.15, offset=0.02, color=NA, colnames_angle=45, font.size=2) +
+  scale_fill_manual(values = c("#4D4D4D", "#0072B2"), name="EFN")
+
+# Third heatmap (another new scale)
+p4 <- gheatmap(p3 + new_scale_fill(), tree_data_2, width=0.15, offset=14, color=NA, colnames_angle=45, font.size=2) +
+  scale_fill_manual(values = c("#4D4D4D", "#C44E52"), name="Rhizobia")
+p4
+
+ggsave("phylogeny/tree_heatmap.pdf", p4)
