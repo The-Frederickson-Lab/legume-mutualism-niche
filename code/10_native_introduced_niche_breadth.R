92a46c2cc398ade5990734378e33a3cbb52d0acc
drfreder <m.frederickson@utoronto.ca>
2026-04-30 14:04:29 -0700

    Fix native/introduced analysis

--- code/10_native_introduced_niche_breadth.R
+++ code/10_native_introduced_niche_breadth.R
@@ -113,6 +113,11 @@ hist(residuals(precip_range))
 # Save as RDS file
 saveRDS(precip_range, here("model_fits/precip_niche_breadth_filters_native.rds"))
 
+# save model output
+precip<-data.frame(coef(summary(precip_range))) %>% format(scientific=F)
+precip$p.value<-as.numeric(precip$p.value) %>% round(4)
+write.csv(precip, here("tables/native_precip_allspecies_output_table.csv"))
+
 # PGLS for temp range ----
 set.seed(10)
 temp_range <- gls(log(temp_range) ~ EFN*abs_med_lat + fixer*abs_med_lat + 
@@ -127,6 +132,10 @@ hist(residuals(temp_range))
 # Save as RDS file
 saveRDS(temp_range, here("model_fits/temp_niche_breadth_filters_native.rds"))
 
+# save model output
+temp<-data.frame(coef(summary(temp_range))) %>% format(scientific=F)
+temp$p.value<-as.numeric(temp$p.value) %>% round(4)
+write.csv(temp, here("tables/native_temp_allspecies_output_table.csv"))
 
 # PGLS for nitrogen range ----
 set.seed(10)
@@ -141,6 +150,11 @@ hist(residuals(nitro_range))
 
 # Save as RDS file
 saveRDS(nitro_range, here("model_fits/nitro_niche_breadth_filters_native.rds"))
+
+# save model output
+nitro<-data.frame(coef(summary(nitro_range))) %>% format(scientific=F)
+nitro$p.value<-as.numeric(nitro$p.value) %>% round(4)
+write.csv(nitro, here("tables/native_nitro_allspecies_output_table.csv"))
 
 #Next model just niche breadth in the introduced range
 
@@ -199,6 +213,17 @@ nat_niche$abs_med_lat <- abs(nat_niche$median_lat)
 kable(intro_niche %>% group_by(EFN) %>% summarize(n=n()))
 kable(intro_niche %>% group_by(fixer) %>% summarize(n=n()))
 
+#Calculate whether mutualists are more or less likely to be introduced than other legumes
+#Chi-squared
+EFNtable <- as.table(rbind(c(247, 62), c(2156, 203)))
+dimnames(EFNtable) <- list(introduced= c(1,0), EFN = c(0,1))
+chisq.test(EFNtable)
+
+#Chi-squared
+Nodtable <- as.table(rbind(c(280, 29), c(2116, 243)))
+dimnames(Nodtable) <- list(introduced= c(1,0), Nod = c(1,0))
+chisq.test(Nodtable)
+
 # PGLS of introduced precip breadth ----
 #hist(log(intro_niche$precip_range))
 
@@ -217,7 +242,6 @@ hist(residuals(intro_precip_range))
 precip_intro<-data.frame(coef(summary(intro_precip_range))) %>% format(scientific=F)
 precip_intro$p.value<-as.numeric(precip_intro$p.value) %>% round(4)
 write.csv(precip_intro, "tables/intro_precip_output_table.csv")
-
 
 # PGLS of introduced temp breadth ----
 
@@ -305,278 +329,4 @@ hist(residuals(nat_nitro_range))
 nitro_nat<-data.frame(coef(summary(nat_nitro_range))) %>% format(scientific=F)
 nitro_nat$p.value<-as.numeric(nitro_nat$p.value) %>% round(4)
 write.csv(nitro_nat, "tables/native_nitro_output_table.csv")
-
-
-# Running on both combined ----
-
-#Calculate niche breadth
-summary_df <- points %>% 
-  st_drop_geometry() %>%
-  #Initial filter: filter to species in the introduced niche breadth analysis
-  filter(species %in% intro_niche$species) %>%
-  #First filter: no points from biome 98 (lake) and 99 (rock and ice)
-  filter(biome != "98" & biome != "99") %>%
-  #Second filter: species must have Plants of the World polygons
-  filter(species %in% poly_sf$spcs_nm) %>%
-  #Third filter: no points from species with less than 50% of points matching POW polygons
-  filter(species %in% subset(summary_spatial_df, percent_in_poly >= 50)$species) %>%
-  group_by(species) %>% 
-  #Fourth filter: filter out species with fewer than 25 total occurrences
-  filter(n() >= 25) %>%
-  reframe(n = n(),
-          precip_maxquant = quantile(precip, 0.95, na.rm=T), 
-          precip_minquant = quantile(precip, 0.05, na.rm=T),
-          precip_mean = mean(precip, na.rm=T),
-          precip_median = median(precip, na.rm=T),
-          nitro_maxquant = quantile(nitrogen, 0.95, na.rm=T),
-          nitro_minquant = quantile(nitrogen, 0.05, na.rm=T),
-          nitro_mean = mean(nitrogen, na.rm=T),
-          nitro_median = median(nitrogen, na.rm=T),
-          temp_maxquant = quantile(temp, 0.95, na.rm=T),
-          temp_minquant = quantile(temp, 0.05, na.rm=T),
-          temp_mean = mean(temp, na.rm=T),
-          temp_median = median(temp, na.rm=T),
-          max_lat = max(Y, na.rm=T),
-          min_lat = min(Y, na.rm=T),
-          mean_lat = mean(Y, na.rm=T),
-          median_lat = median(Y, na.rm=T),
-          median_long = median(X, na.rm=T),
-          quant95 = quantile(Y, 0.95, na.rm=T),
-          quant005 = quantile(Y, 0.05, na.rm=T),
-          num_biome = length(unique(na.omit(biome))),
-          biome = names(which.max(table(na.omit(biome))))
-  ) %>% 
-  mutate(precip_range = precip_maxquant - precip_minquant,
-         temp_range = temp_maxquant - temp_minquant,
-         nitro_range = nitro_maxquant - nitro_minquant
-  )
-
-total_traits<-left_join(summary_df, traits, 
-                        join_by(species==species), multiple="any")
-
-# make sure that R knows our categorical variables are factors
-total_traits$EFN<-as.factor(total_traits$EFN)
-total_traits$fixer<-as.factor(total_traits$fixer)
-
-# add in absolute median latitude
-total_traits$abs_med_lat<-abs(total_traits$median_lat)
-
-# PGLS for nat + intro precip breadth ----
-total_precip_range <- gls(log(precip_range) ~ EFN*abs_med_lat+
-                            fixer*abs_med_lat+woody+uses_num_uses+annual,
-                          data=total_traits, 
-                          correlation=corPagel(0.51, tree_pruned, form=~species, fixed=TRUE),
-                          method="ML")
-summary(total_precip_range)
-
-plot(total_precip_range)
-qqnorm(total_precip_range, abline = c(0,1))
-hist(residuals(total_precip_range))
-
-# save model output
-precip_total<-data.frame(coef(summary(total_precip_range))) %>% format(scientific=F)
-precip_total$p.value<-as.numeric(precip_total$p.value) %>% round(4)
-write.csv(precip_total, "tables/nat_intro_precip_output_table.csv")
-
-# PGLS for nat + intro temp breadth ----
-total_temp_range <- gls(log(temp_range) ~ EFN*abs_med_lat+
-                          fixer*abs_med_lat+woody+uses_num_uses+annual,
-                        data=total_traits, 
-                        correlation=corPagel(0.60, tree_pruned, form=~species, fixed=TRUE),
-                        method="ML")
-
-summary(total_temp_range)
-
-plot(total_temp_range)
-qqnorm(total_temp_range, abline = c(0,1))
-hist(residuals(total_temp_range))
-
-temp_total<-data.frame(coef(summary(total_temp_range))) %>% format(scientific=F)
-temp_total$p.value<-as.numeric(temp_total$p.value) %>% round
-write.csv(temp_total, "tables/nat_intro_temp_output_table.csv")
-
-# PGLS for nat + intro nitro breadth ----
-total_nitro_range <- gls(log(nitro_range) ~ EFN*abs_med_lat+
-                           fixer*abs_med_lat+woody+uses_num_uses+annual,
-                         data=total_traits, 
-                         correlation=corPagel(0.55, tree_pruned, form=~species, fixed=TRUE),
-                         method="ML")
-
-summary(total_nitro_range)
-
-plot(total_nitro_range)
-qqnorm(total_nitro_range, abline = c(0,1))
-hist(residuals(total_nitro_range))
-
-nitro_total<-data.frame(coef(summary(total_nitro_range))) %>% format(scientific=F)
-nitro_total$p.value<-as.numeric(nitro_total$p.value) %>% round(4)
-write.csv(nitro_total, "tables/nat_intro_nitro_output_table.csv")
-
-
-# making native vs. introduced figures ----
-
-
-# add nat_ to each column in native df
-colnames(nat_niche) <- paste0('nat_', colnames(nat_niche))
-colnames(total_traits)<-paste0('tot_', colnames(total_traits))
-
-# combine dataframes
-combine<-left_join(nat_niche, intro_niche, join_by(nat_species==species), multiple="any")
-combine1<-left_join(combine, total_traits, join_by(nat_species==tot_species), multiple="any")
-
-
-# shorter version of dataframe with just what we need
-data_short<-combine1 %>% dplyr::select(nat_species, precip_range,
-                                       temp_range, nitro_range,
-                                       nat_precip_range, nat_temp_range,
-                                       nat_nitro_range, nat_EFN, nat_Domatia, nat_fixer,
-                                       tot_precip_range, tot_temp_range, tot_nitro_range)
-
-# use melt to make the dataframe longer (this way, can graph with
-# multiple measures on same axes)
-data_melt<-reshape2::melt(data_short, id.vars=c("nat_species", "nat_EFN", "nat_Domatia", "nat_fixer"),
-                          measure.vars=c("precip_range",
-                                         "temp_range", "nitro_range",
-                                         "nat_precip_range", "nat_temp_range",
-                                         "nat_nitro_range", "tot_precip_range",
-                                         "tot_nitro_range", "tot_temp_range"))
-
-data_melt$nat_EFN<-as.factor(data_melt$nat_EFN)
-data_melt$nat_Domatia<-as.factor(data_melt$nat_Domatia)
-data_melt$nat_fixer<-as.factor(data_melt$nat_fixer)
-data_melt$variable<-as.factor(data_melt$variable)
-
-# EFN plot ----
-
-EFN_temp<-data_melt %>% 
-  subset(variable=="temp_range" | variable=="nat_temp_range" | variable=="tot_temp_range") %>% 
-  group_by(nat_EFN) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_EFN)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("EFN")+
-  ylab("Mean annual\ntemp. range (\u00B0C)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#B50A2AFF"), labels=c("no", "yes"))+
-  # theme(legend.position="none")+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  labs(fill='EFN'); EFN_temp
-
-EFN_precip<-data_melt %>% 
-  subset(variable=="precip_range" | variable=="nat_precip_range" | variable=="tot_precip_range") %>% 
-  group_by(nat_EFN) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_EFN)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("EFN")+
-  ylab("Annual precip.\nrange (mm)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#B50A2AFF"), labels=c("no", "yes"))+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  labs(fill='EFN'); EFN_precip
-
-EFN_nitro<-data_melt %>% 
-  subset(variable=="nitro_range" | variable=="nat_nitro_range" | variable=="tot_nitro_range") %>% 
-  group_by(nat_EFN) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_EFN)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("EFN")+
-  ylab("Soil nitrogen\nrange (cg/kg)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#B50A2AFF"), labels=c("no", "yes"))+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  guides(fill = guide_legend(nrow = 1))+
-  labs(fill='EFN'); EFN_nitro
-
-# fixer plots ----
-
-fixer_temp<-data_melt %>% 
-  subset(variable=="temp_range" | variable=="nat_temp_range" | variable=="tot_temp_range") %>% 
-  group_by(nat_fixer) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_fixer)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("Fixer")+
-  ylab("Mean annual\ntemp. range (\u00B0C)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#26432FFF"))+
-  theme(axis.title.y=element_blank())+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  labs(fill='Rhizobia'); fixer_temp
-
-fixer_precip<-data_melt %>% 
-  subset(variable=="precip_range" | variable=="nat_precip_range" | variable=="tot_precip_range") %>% 
-  group_by(nat_fixer) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_fixer)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("Fixer")+
-  ylab("Annual precip.\nrange (mm)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#26432FFF"))+
-  theme(legend.position="none", axis.title.y=element_blank(), axis.title.x=element_blank())+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  labs(fill='Rhizobia'); fixer_precip
-
-fixer_nitro<-data_melt %>% 
-  subset(variable=="nitro_range" | variable=="nat_nitro_range" | variable=="tot_nitro_range") %>% 
-  group_by(nat_fixer) %>%
-  #summarise(average = mean(value)) %>% 
-  ggplot()+
-  aes(x=variable, y=value, fill=nat_fixer)+
-  geom_boxplot(stat="boxplot")+
-  theme_classic()+
-  xlab("Fixer")+
-  ylab("Soil nitrogen\nrange (cg/kg)")+
-  scale_x_discrete(labels=c("introduced", "native", "total"))+
-  scale_fill_manual(values=c("#92BBD9FF", "#26432FFF"))+
-  theme(legend.position="none", axis.title.y=element_blank(), axis.title.x=element_blank())+
-  theme(axis.title.x=element_blank(), axis.title=element_text(size=12), 
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12))+
-  labs(fill='Rhizobia')+
-  guides(fill = guide_legend(nrow = 1)); fixer_nitro
-
-
-legend_efn <- cowplot::get_legend(EFN_temp)
-legend_fixer<-cowplot::get_legend(fixer_temp)
-legends<-cowplot::plot_grid(legend_efn, legend_fixer, ncol=1, nrow=2)
-
-
-
-P<-cowplot::plot_grid(EFN_temp+ theme(legend.position="none"),  
-                      fixer_temp+ theme(legend.position="none"),
-                      legends,
-                      EFN_precip+ theme(legend.position="none"), fixer_precip+ theme(legend.position="none"), NA,
-                      EFN_nitro+ theme(legend.position="none"), fixer_nitro+ theme(legend.position="none"), NA,
-                      ncol=3, nrow=3,
-                      labels = c('A', 'D', '', 'B', 'E', '', 'C', 'F', ''),
-                      label_size = 14,
-                      label_x = c(0.05, -0.05, 0, 0.05, -0.05, 0, 0.05, -0.05, 0),
-                      align = "hv", axis = "lrtb", rel_widths = c(1,1,0.5)); P
-
-ggsave("figures/introduced_vs_native_breadths.jpg", height = 10, width = 9)
-ggsave("figures/introduced_vs_native_breadths.pdf", height = 10, width = 9)
-
 
