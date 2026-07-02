1af064e68eaf6f18f98d83d452ffc2bbe5385895
drfreder <m.frederickson@utoronto.ca>
2026-07-01 20:12:32 -0700

    Add Wallacean shortfall to models

--- code/08_models_with_4filters.R
+++ code/08_models_with_4filters.R
@@ -1,5 +1,6 @@
 #Packages
 library(tidyverse)
+library(ggeffects)
 library(raster)
 library(sf)
 library(here)
@@ -128,6 +129,21 @@ filter(n() >= 25) %>%
          nitro_range = nitro_maxquant - nitro_minquant
          )
 
+#Calculate Wallacean darkspot score for each species
+darkspot_df <- read.csv(here("data/nph20024-sup-0004-tables3.csv"))
+darkspot_df_meta <- read.csv(here("data/nph20024-sup-0002-tables1.csv"))
+darkspot_df <- merge(darkspot_df, darkspot_df_meta[, c(2:3)], by.x="Botanical.country", by.y="LEVEL3_NAM", all.x=T, all.y=T)
+unique(points_joined$ar_cd_3) %in% unique(darkspot_df$LEVEL3_COD) #All points are in polygons within the dark spot dataset
+points_joined <- merge(points_joined, darkspot_df[, c("wallacean_darkspot_score", "LEVEL3_COD")], by.x="ar_cd_3", by.y="LEVEL3_COD", all.x=T, all.y=F)
+
+species_wal_score <- points_joined %>% 
+  st_drop_geometry() %>%
+  group_by(species) %>%
+  summarize(mean_wal_score = mean(wallacean_darkspot_score, na.rm=T))
+
+#Add to species-level summary
+summary_df <- merge(summary_df, species_wal_score, by.x="species", by.y="species")
+
 #Read in trait data
 traits <- read.csv(here("data/updated_legume_range_traits.csv")) %>% 
   #rename(species = Phy) %>% #Don't need this for updated trait data
@@ -160,12 +176,161 @@ write.csv(data, "data/pgls_species_data.csv")
 #Summarize number of species in different groups
 kable(data %>% group_by(fixer, EFN) %>% summarize(n=n()))
 
+#Summarize distribution of occurrence records by species
+hist(log(data$n))
+mean(data$n)
+sd(data$n)/sqrt(length(data$n))
+
+#Make mutualism traits factors
+data$EFN <- as.factor(data$EFN)
+data$fixer <- as.factor(data$fixer)
+
+#Plots of niche breadth versus occurrence counts
+p1 <- ggplot(data=data,aes(x=n, y=precip_range))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  theme_cowplot()+
+  xlab("Occurrences (no.)")+
+  ylab("Precip. breadth (mm)")+
+  scale_x_log10()+
+  scale_y_log10()
+
+p2 <- ggplot(data=data,aes(x=n, y=temp_range))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  theme_cowplot()+
+  xlab("Occurrences (no.)")+
+  ylab("Temp. breadth (\u00B0C)\n")+
+  scale_x_log10()+
+  scale_y_log10()
+
+p3 <- ggplot(data=data,aes(x=n, y=nitro_range, color=human_uses, group=human_uses))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  theme_cowplot()+
+  xlab("Occurrences (no.)")+
+  ylab("Soil N breadth (cg/kg)")+
+  scale_x_log10()+
+  scale_y_log10()
+
+p4 <- plot_grid(p1, p2, p3, nrow=1, labels="AUTO")
+p4
+save_plot(here("figures/niche_breadth_by_occ_count.pdf"), p4, base_width =10, base_height=5)
+
+#Plot of number of human uses versus occurrence counts
+hist(data$uses_num_uses, breaks=10)
+kable(data %>% group_by(uses_num_uses) %>% summarize(n=n()))
+
+p5 <- ggplot(data=data,aes(x=n, y=uses_num_uses))+geom_point(alpha=0.3)+geom_smooth(method="lm")+
+  theme_cowplot()+xlab("Occurrences (no.)")+ylab("Human uses (no)")+scale_x_log10()
+p5
+
+cor(data$uses_num_uses, data$n)
+summary(lm(n~uses_num_uses, data=data))
+
+#Do mutualists have more occurrences than non-mutualists?
+p6 <- ggplot(data=data, aes(x=EFN, y=n, color=EFN, fill=EFN))+
+  #geom_jitter(alpha=0.1)+
+  geom_violin(draw_quantiles = c(0.5), alpha=0.5)+
+  theme_cowplot()+
+  scale_y_log10()+
+  scale_colour_manual(values=c("#4D4D4D", "#0072B2"), labels=c("No", "Yes"))+
+  scale_fill_manual(values=c("#4D4D4D", "#0072B2"), labels=c("No", "Yes"))+
+  ylab("Occurrences (no.)")+
+  xlab("EFN")+
+  scale_x_discrete(labels = c("0" = "No", "1" = "Yes"))
+
+p7 <- ggplot(data=data, aes(x=fixer, y=n, color=fixer, fill=fixer))+
+  #geom_jitter(alpha=0.1)+
+  geom_violin(draw_quantiles = c(0.5), alpha=0.5)+
+  theme_cowplot()+
+  scale_y_log10()+
+  scale_colour_manual(values=c("#4D4D4D", "#C44E52"), labels=c("No", "Yes"))+
+  scale_fill_manual(values=c("#4D4D4D", "#C44E52"), labels=c("No", "Yes"))+
+  ylab("Occurrences (no.)")+
+  xlab("Rhizobia")+
+  scale_x_discrete(labels = c("0" = "No", "1" = "Yes"))+
+  labs(color="Rhizobia")+
+  labs(fill="Rhizobia")
+
+p8 <- plot_grid(p6, p7, nrow=1, labels="AUTO")
+save_plot(here("figures/occ_count_by_mutualism.pdf"), p8, base_width =8, base_height=4)
+
+#Wallacean shortfall figures
+p9 <- ggplot(data=data, aes(x=EFN, y=mean_wal_score, color=EFN, fill=EFN))+
+  #geom_jitter(alpha=0.1)+
+  geom_violin(draw_quantiles = c(0.5), alpha=0.5)+
+  theme_cowplot()+
+  scale_colour_manual(values=c("#4D4D4D", "#0072B2"), labels=c("No", "Yes"))+
+  scale_fill_manual(values=c("#4D4D4D", "#0072B2"), labels=c("No", "Yes"))+
+  ylab("Wallacean shortfall (score)")+
+  xlab("EFN")+
+  scale_x_discrete(labels = c("0" = "No", "1" = "Yes"))
+
+p10 <- ggplot(data=data, aes(x=fixer, y=mean_wal_score, color=fixer, fill=fixer))+
+  #geom_jitter(alpha=0.1)+
+  geom_violin(draw_quantiles = c(0.5), alpha=0.5)+
+  theme_cowplot()+
+  scale_colour_manual(values=c("#4D4D4D", "#C44E52"), labels=c("No", "Yes"))+
+  scale_fill_manual(values=c("#4D4D4D", "#C44E52"), labels=c("No", "Yes"))+
+  ylab("Wallacean shortfall (score)")+
+  xlab("Rhizobia")+
+  scale_x_discrete(labels = c("0" = "No", "1" = "Yes"))+
+  labs(color="Rhizobia")+
+  labs(fill="Rhizobia")
+
+p11 <- plot_grid(p9, p10, nrow=1, labels="AUTO")
+save_plot(here("figures/wallacean_score_by_mutualism.pdf"), p11, base_width =8, base_height=4)
+
+summary(gls(mean_wal_score ~ EFN*abs_med_lat+fixer*abs_med_lat 
+            + woody + uses_num_uses
+            + annual,
+            data = data, 
+            correlation=corPagel(1, mytree, form=~species), method="ML"))
+
+summary(gls(log(n) ~ EFN*abs_med_lat+fixer*abs_med_lat 
+            + woody + uses_num_uses
+            + annual,
+            data = data, 
+            correlation=corPagel(1, mytree, form=~species), method="ML"))
+
+p12 <- ggplot(data=data,aes(x=mean_wal_score, y=n))+geom_point(alpha=0.5)+geom_smooth(method="lm")+
+  theme_cowplot()+ylab("Occurrences (no.)")+xlab("Wallacean shortfall (score)")+scale_y_log10()
+save_plot(here("figures/wal_score_n.pdf"), p12)
+
+p13 <-  ggplot(data=data,aes(x=mean_wal_score, y=nitro_range))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  scale_y_log10()+
+  theme_cowplot()+
+  xlab("Wallacean shortfall (score)")+
+  ylab("Soil N breadth (cg/kg)")
+  
+p14 <- ggplot(data=data,aes(x=mean_wal_score, y=precip_range))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  scale_y_log10()+
+  theme_cowplot()+
+  xlab("Wallacean shortfall (score)")+
+  ylab("Precip. breadth (mm)")
+
+p15 <- ggplot(data=data,aes(x=mean_wal_score, y=temp_range))+
+  geom_point(alpha=0.3)+
+  geom_smooth(method="lm")+
+  scale_y_log10()+
+  theme_cowplot()+
+  xlab("Wallacean shortfall (score)")+
+  ylab("Temp. breadth (\u00B0C)\n")
+
+p16 <- plot_grid(p14, p15, p13, nrow=1, labels="AUTO")
+save_plot(here("figures/wal_score_niche.pdf"), p16, base_width=10, base_height=5)
+
 #Biome number model
 
 # PGLS with biome as response variable ----
 biome_number <- gls(num_biome ~ EFN*abs_med_lat+fixer*abs_med_lat 
                     + woody + uses_num_uses
-                    + annual,
+                    + annual + mean_wal_score,
                     data = data, 
                     correlation=corPagel(1, mytree, form=~species), method="ML")
 summary(biome_number)
@@ -198,7 +363,7 @@ write.csv(fixer_biome_means, here("tables/biome_fixer_predictions.csv"), row.nam
 # PGLS for precip range ----
 set.seed(10)
 precip_range <- gls(log(precip_range) ~ EFN*abs_med_lat + fixer*abs_med_lat+
-                      woody + uses_num_uses + annual,
+                      woody + uses_num_uses + annual + mean_wal_score,
                     data = data, 
                     correlation = corPagel(1, tree_pruned, form=~species), method = "ML")
 summary(precip_range)
@@ -231,7 +396,7 @@ set.seed(10)
 
 #Not log-transformed
 temp_range <- gls(temp_range ~ EFN*abs_med_lat + fixer*abs_med_lat + 
-                      woody + uses_num_uses + annual,
+                      woody + uses_num_uses + annual + mean_wal_score,
                     data = data, 
                     correlation = corPagel(1, mytree, form=~species), method = "ML")
 
@@ -250,7 +415,7 @@ write.csv(temp_df, here("tables/temp_breadth_output_table.csv"))
 
 #Log-transformed
 logtemp_range <- gls(log(temp_range) ~ EFN*abs_med_lat + fixer*abs_med_lat + 
-                    woody + uses_num_uses + annual,
+                    woody + uses_num_uses + annual + mean_wal_score,
                   data = data, 
                   correlation = corPagel(1, mytree, form=~species), method = "ML")
 
@@ -279,11 +444,10 @@ plot(fixer_temp_means)
 write.csv(EFN_temp_means, here("tables/temp_range_EFN_predictions.csv"), row.names = FALSE)
 write.csv(fixer_temp_means, here("tables/temp_range_fixer_predictions.csv"), row.names = FALSE)
 
-
 # PGLS for nitrogen range ---
 set.seed(10)
 nitro_range <- gls(log(nitro_range) ~ EFN*abs_med_lat+fixer*abs_med_lat + 
-                     woody + uses_num_uses + annual,
+                     woody + uses_num_uses + annual+mean_wal_score,
                    data = data, 
                    correlation = corPagel(1, mytree, form=~species), method = "ML")
 
